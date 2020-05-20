6ea7b5f9e1fb44c3f554ea938ebb0b9e2a3a630d
Simon Thordal <simonthordal@gmail.com>
2020-05-20 14:39:46 +0200

    5.7 migrator uses setOuput and not getNotes

--- src/Console/Migrations/MigrateCommand.php
+++ src/Console/Migrations/MigrateCommand.php
@@ -54,21 +54,14 @@ class MigrateCommand extends BaseCommand
         }
 
         // The pretend option can be used for "simulating" the migration and grabbing
-        // the SQL queries that would fire if the migration were to be run against
+        // the Cypher queries that would fire if the migration were to be run against
         // a database for real, which is helpful for double checking migrations.
         $pretend = $this->input->getOption('pretend');
-
+        $this->migrator->setOutput($this->output);
         $path = $this->getMigrationPath();
 
         $this->migrator->setConnection($this->input->getOption('database'));
         $this->migrator->run($path, ['pretend' => $pretend]);
-
-        // Once the migrator has run we will grab the note output and send it out to
-        // the console screen, since the migrator itself functions without having
-        // any instances of the OutputInterface contract passed into the class.
-        foreach ($this->migrator->getNotes() as $note) {
-            $this->output->writeln($note);
-        }
 
         // Finally, if the "seed" option has been given, we will re-run the database
         // seed task to re-populate the database, which is convenient when adding
@@ -76,27 +69,5 @@ class MigrateCommand extends BaseCommand
         if ($this->input->getOption('seed')) {
             $this->call('db:seed', ['--force' => true]);
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function getOptions()
-    {
-        return array(
-            array('bench', null, InputOption::VALUE_OPTIONAL, 'The name of the workbench to migrate.', null),
-
-            array('database', null, InputOption::VALUE_OPTIONAL, 'The database connection to use.'),
-
-            array('force', null, InputOption::VALUE_NONE, 'Force the operation to run when in production.'),
-
-            array('path', null, InputOption::VALUE_OPTIONAL, 'The path to migration files.', null),
-
-            array('package', null, InputOption::VALUE_OPTIONAL, 'The package to migrate.', null),
-
-            array('pretend', null, InputOption::VALUE_NONE, 'Dump the SQL queries that would be run.'),
-
-            array('seed', null, InputOption::VALUE_NONE, 'Indicates if the seed task should be re-run.'),
-        );
     }
 }
